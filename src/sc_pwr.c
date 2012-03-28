312df5b45dae3afcad507be3308c18edaaecda0a
Tuomas Kulve <tuomas.kulve@snowcap.fi>
2012-03-28 19:54:06 +0300

    Add power related functionality.

---
+++ src/sc_pwr.c
@@ -0,0 +1,73 @@
+/***
+ * Run mode / power consumption functions
+ *
+ * Copyright 2011 Tuomas Kulve, <tuomas.kulve@snowcap.fi>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+
+#include "sc_utils.h"
+#include "sc_pwr.h"
+
+
+/*
+ * Put the MCU to StandBy mode (all off except RTC)
+ */
+void sc_pwr_standby(void)
+{
+  chSysLock();
+
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+  PWR->CR |= (PWR_CR_PDDS | PWR_CR_CSBF | PWR_CR_CWUF);
+  RTC->ISR &= ~(RTC_ISR_ALRBF | RTC_ISR_ALRAF | RTC_ISR_WUTF | RTC_ISR_TAMP1F | RTC_ISR_TSOVF | RTC_ISR_TSF);
+
+  __WFI();
+}
+
+
+
+/*
+ * Set periodic wakeup
+ */
+void sc_pwr_wakeup_set(uint32_t sec, uint32_t ms)
+{
+  RTCWakeup wakeupspec;
+ 
+  wakeupspec.wakeup = ((uint32_t)4) << 16; /* select 1 Hz clock source */
+
+  /* FIXME: No idea why this doesn't match to 1Hz */
+  wakeupspec.wakeup |= 30 * sec;
+
+  rtcSetPeriodicWakeup_v2(&RTCD1, &wakeupspec);
+}
+
+
+
+/*
+ * Set periodic wakeup
+ */
+void sc_pwr_wakeup_clear(void)
+{
+  rtcSetPeriodicWakeup_v2(&RTCD1, NULL);
+}
