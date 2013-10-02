ba4abf987fa805979f19cafc8f1d327af7dbdbba
Tuomas Kulve <tuomas.kulve@snowcap.fi>
2013-10-02 20:50:49 +0300

    Add compile-all-test.sh
    Run compile-all-test.sh when changing the framework to be sure no
    application gets broken.

---
+++ compile-all-test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+PROJECTS="test pleco"
+BOARDS="SC_SNOWCAP_V1 SC_F4_DISCOVERY SC_F1_DISCOVERY"
+
+for project in $PROJECTS
+do
+  for board in $BOARDS
+  do
+    SC_PROJECT=$project SC_BOARD=$board make clean
+	echo "BUILDING PROJECT $project ON BOARD $board"
+    SC_PROJECT=$project SC_BOARD=$board make -j4
+  done
+done
