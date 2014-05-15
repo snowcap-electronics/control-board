215c1db866952a28c7a4aa6e8e8385ef0a8cd11a
Tuomas Kulve <tuomas.kulve@snowcap.fi>
2014-05-15 19:42:33 +0300

    build: Add option to store all binaries.

--- compile-all-test.sh
+++ compile-all-test.sh
@@ -10,4 +10,12 @@ do
   make SC_PROJECT_CONFIG=$cfg clean
   echo "BUILDING PROJECT CONFIG $cfg"
   make SC_PEDANTIC_COMPILER=1 SC_PROJECT_CONFIG=$cfg -j4
+
+  if [ "$1" == "store" ]
+  then
+    config_name=`basename $cfg .mk`
+    rm -rf out/$config_name
+    mkdir -p out/$config_name
+    cp build/*{bin,dmp,elf,hex,map} out/$config_name/
+  fi
 done
