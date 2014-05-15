5e229ac4d2d6e37d7caea4a434c38cec8741c865
Tuomas Kulve <tuomas.kulve@snowcap.fi>
2014-05-15 20:23:21 +0300

    build: Fix string compare in compile all script.

--- compile-all-test.sh
+++ compile-all-test.sh
@@ -11,7 +11,7 @@ do
   echo "BUILDING PROJECT CONFIG $cfg"
   make SC_PEDANTIC_COMPILER=1 SC_PROJECT_CONFIG=$cfg -j4
 
-  if [ "$1" == "store" ]
+  if [ "$1" = "store" ]
   then
     config_name=`basename $cfg .mk`
     rm -rf out/$config_name
