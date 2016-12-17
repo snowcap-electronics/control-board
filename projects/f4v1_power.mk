9d2cacebbc1f41a30eb1024e77f08b37d0be714a
Tuomas Kulve <tuomas.kulve@snowcap.fi>
2016-12-17 18:45:35 +0200

    Add a bunch of forgotten project files

---
+++ projects/f4v1_power.mk
@@ -0,0 +1,24 @@
+SC_PROJECT = power
+SC_BOARD = SC_SNOWCAP_STM32F4_V1
+
+# Disable ChibiOS HAL features
+SC_DEFINES = -DHAL_USE_ADC=FALSE -DHAL_USE_CAN=FALSE -DHAL_USE_ICU=FALSE -DHAL_USE_MAC=FALSE -DHAL_USE_MMC_SPI=FALSE -DHAL_USE_PWM=FALSE
+
+# Disable ChibiOS features not used currently by Snowcap projects
+# vsnprintf with %f seems to need CH_USE_MEMCORE, CH_USE_HEAP and CH_USE_DYNAMIC
+SC_DEFINES += -DCH_USE_CONDVARS=FALSE -DCH_USE_MESSAGES=FALSE -DCH_USE_MEMPOOLS=FALSE
+
+# Enable float type support in ch*printf
+SC_DEFINES += -DCHPRINTF_USE_FLOAT=TRUE
+
+# Increase the size of maximum UART message
+SC_DEFINES += -DUART_MAX_SEND_BUF_LEN=254
+
+# Increase the event queue size and the event loop thread's work area
+SC_DEFINES += -DEVENT_MB_SIZE=512 -DSC_EVENT_LOOP_THREAD_WA=4096
+
+# Increase the number of USB serial send buffers
+SC_DEFINES += -DSDU_MAX_SEND_BUFFERS=8
+
+# Enable WFI in idle thread
+#SC_DEFINES += -DCORTEX_ENABLE_WFI_IDLE=TRUE
---
+++ projects/f4v1_wireless_ldr.mk
@@ -0,0 +1,28 @@
+SC_PROJECT = wireless-ldr
+SC_BOARD = SC_SNOWCAP_STM32F4_V1
+
+# Use 25 Mhz MCU clock instead of 168 Mhz to save power
+SC_DEFINES += -DSC_MCU_LOW_SPEED=1
+
+# Use Spirit1 with ST licensed code
+SC_DEFINES += -DSC_ALLOW_ST_LIBERTY_V2=1 -DSC_HAS_SPIRIT1=1
+
+SC_EXTRA_CSRC = \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Aes.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Calibration.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Commands.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Csma.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_DirectRF.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_General.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Gpio.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Irq.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_LinearFifo.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Management.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_PktBasic.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_PktCommon.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_PktMbus.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_PktStack.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Qi.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Radio.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Timer.c \
+			drivers/SPIRIT1_Library/Src/SPIRIT_Types.c
